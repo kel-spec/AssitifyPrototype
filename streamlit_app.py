8f86d295f52bd261ac6c8d6f331553fc63dd194e
kel-spec <qmlflagunday@tip.edu.ph>
2024-11-29 12:52:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from textblob import TextBlob
 
 # Set page config to make it more chat-like
-st.set_page_config(page_title="Assistify", layout="wide")
+st.set_page_config(page_title="Assistify 🛒", layout="wide")
 
 # Define chatbot responses
 responses = {
